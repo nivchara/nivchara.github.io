149dec87dd008eea64c28d6c3982737b5bf90c5f
KRBY <krwylie18@gmail.com>
2022-06-10 01:28:17 -0700

    changed images to be all vertical, started js

---
+++ gallery/gallery.js
@@ -0,0 +1,18 @@
+const track = document.querySelector('carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const previousButton = document.querySelector('.carousel__button--left');
+const dotNav = document.querySelector('.carousel__nav');
+const dot = array.from(dotNav.children);
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+//arrange slides next to one another
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+slides.forEach((slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+})
+
+//when clicking left icon, move slides left as well as right on right icon click. 
+//when clicking nav indicators, move to that slide.
